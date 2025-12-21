03c42cd2f8cfc3a1b2333f0fce5157e1523a607f
SupeemAFK <peemlomsak@gmail.com>
2025-12-21 12:37:22 +0000

    update productpage and details

--- src/details/details.rb
+++ src/details/details.rb
@@ -69,7 +69,7 @@ def get_book_details(id, headers)
         'author': 'William Shakespeare',
         'year': 1595,
         'type' => 'paperback',
-        'pages' => 5020,
+        'pages' => 200,
         'publisher' => 'PublisherA',
         'language' => 'English',
         'ISBN-10' => '1234567890',
